399b5d95a0fafe6a25d0ab137bda3853a40d2603
SivaprakashRaman <prakashraman441@gmail.com>
2025-10-31 10:36:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Create from './components/Create';
 import Read from './components/Read';
 import Update from './components/Updated';
-import NotFound from './components/NotFound';
+//import NotFound from './components/NotFound';
 import './App.css';
 
 function App() {
@@ -20,9 +20,6 @@ function App() {
               </Routes>
               <Routes>
                 <Route exact path='/update' Component={Update} />
-              </Routes>
-              <Routes>
-                <Route path="*" Component={NotFound} /> 
               </Routes>
             </div>
           </div>
